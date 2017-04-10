2ca515b4ade2730c0f1433ca180fdfe9b94f748c
JaydenSoong <jiandongsong@126.com>
2017-04-10 18:16:33 +0800

    Add ActivityTest

---
+++ ActivityTest/app/src/main/java/com/example/dailiang/activitytest/FirstActivity.java
@@ -0,0 +1,132 @@
+package com.example.dailiang.activitytest;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            case 1:
+                if(resultCode == RESULT_OK){
+                    String returnData = data.getStringExtra("data_return");
+                    Log.d("FirstActivity", returnData);
+                }
+                break;
+            default:
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+        Button button1 = (Button) findViewById(R.id.button_1);
+
+        // 使用显示 Intent 从下一个活动返回数据
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+
+                /**
+                 * startActivityForResult() 期望在活动销毁的时候能够返回一个结果给上一个活动
+                 * @Intent 第一个参数，是一个 Intent
+                 * @requestCode 第二个参数，请求码。用于在之后的回调中判断数据的来源
+                 */
+                startActivityForResult(intent, 1);
+            }
+        });
+        // 使用隐式 Intent 来调用拨号界面
+        /*
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:10086"));
+                startActivity(intent);
+            }
+        });
+        */
+
+        // 使用隐式 Intent 来启动其它活动
+        /*
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.baidu.com"));
+                startActivity(intent);
+            }
+        });
+        */
+
+        // 使用隐式 Intent 启动活动
+        /*
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.example.dailiang.activitytest.ACTION_START");
+                intent.addCategory("com.example.dailiang.activitytest.MY_CATEGO.RY");
+                startActivity(intent);
+            }
+        });
+        */
+
+        // 使用显式 Intent  启动活动，并向下一个活动传递数据
+        /*
+        button1.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                String data = "Hello SecondActivity";
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                intent.putExtra("extra_data", data);
+                startActivity(intent);
+            }
+        });
+        */
+
+        /*
+        button1.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FirstActivity.this, "You clicked Button 1", Toast.LENGTH_SHORT).show();
+                // 通过代码消毁一个活动的方法（与按 Back 键效果相同），下面代码的作用是：当运行程序时，点击
+                // 一下按钮，当前的活动就被销毁了。
+                // finish();
+            }
+        });
+        */
+    }
+}
