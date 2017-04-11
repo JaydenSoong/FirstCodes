4f78db13a4f9621e1371c0762808ab024ddac838
JaydenSoong <jiandongsong@126.com>
2017-04-11 17:20:20 +0800

    EditText 学习

--- UIWidgetTest/app/src/main/java/com/example/dailiang/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/com/example/dailiang/uiwidgettest/MainActivity.java
@@ -4,9 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    //  声明变量 editText
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,8 +25,17 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 在此处添加逻辑
+                // 在此处添加逻辑——显示 EditText 输入的内容
+                String inputText = editText.getText().toString();
+                Toast.makeText(MainActivity.this, inputText, Toast.LENGTH_SHORT).show();
             }
         });
+
+        /*
+         * EditText 学习
+         */
+        // 1. 声明变量 editText, 在 onCreate() 前面
+        // 2. 为 editText 赋值
+        editText = (EditText) findViewById(R.id.edit_text);
     }
 }
