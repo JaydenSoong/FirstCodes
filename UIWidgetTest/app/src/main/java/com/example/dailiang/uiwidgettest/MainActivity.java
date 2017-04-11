fbb558a3762c99327a252d250371cd4b5e99de84
JaydenSoong <jiandongsong@126.com>
2017-04-11 16:55:35 +0800

    Button 学习

--- UIWidgetTest/app/src/main/java/com/example/dailiang/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/com/example/dailiang/uiwidgettest/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.dailiang.uiwidgettest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+         * Button 学习
+         */
+        // 1.创建 Button
+        Button button = (Button) findViewById(R.id.button);
+        // 2.添加逻辑——通过匿名内部类
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 在此处添加逻辑
+            }
+        });
     }
 }
