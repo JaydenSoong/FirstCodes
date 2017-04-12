fd9d942bd4b090652619098bd13decb4ea688979
JaydenSoong <jiandongsong@126.com>
2017-04-12 08:34:56 +0800

    ImageView 学习

--- UIWidgetTest/app/src/main/java/com/example/dailiang/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/com/example/dailiang/uiwidgettest/MainActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     //  声明变量 editText
     private EditText editText;
+    //  声明变量 imageView
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
                 // 在此处添加逻辑——显示 EditText 输入的内容
                 String inputText = editText.getText().toString();
                 Toast.makeText(MainActivity.this, inputText, Toast.LENGTH_SHORT).show();
+
+                // 更换 ImageView 里的图片
+                imageView.setImageResource(R.drawable.wm);
             }
         });
 
@@ -37,5 +43,12 @@ public class MainActivity extends AppCompatActivity {
         // 1. 声明变量 editText, 在 onCreate() 前面
         // 2. 为 editText 赋值
         editText = (EditText) findViewById(R.id.edit_text);
+
+        /*
+         * ImageView 学习
+         */
+        // 1. 声明变量 imageView, 在 onCreate() 前面
+        // 2. 为 imageView 赋值
+        imageView = (ImageView) findViewById(R.id.image_view);
     }
 }
