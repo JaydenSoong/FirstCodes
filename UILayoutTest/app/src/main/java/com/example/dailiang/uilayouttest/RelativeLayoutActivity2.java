cfbe0d10736ea81f50c459db606e5e9c2ae4032e
JaydenSoong <jiandongsong@126.com>
2017-04-14 08:51:15 +0800

    FrameLayout 帧布局学习

--- UILayoutTest/app/src/main/java/com/example/dailiang/uilayouttest/RelativeLayoutActivity2.java
+++ UILayoutTest/app/src/main/java/com/example/dailiang/uilayouttest/RelativeLayoutActivity2.java
@@ -1,7 +1,10 @@
 package com.example.dailiang.uilayouttest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RelativeLayoutActivity2 extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class RelativeLayoutActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_relative_layout2);
+
+        Button button = (Button) findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RelativeLayoutActivity2.this, FrameLayoutActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
