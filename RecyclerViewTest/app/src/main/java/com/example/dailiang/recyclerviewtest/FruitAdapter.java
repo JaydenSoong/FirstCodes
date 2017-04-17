6fa6a8b2515d0b59d65da1c27e9727ea6790665c
JaydenSoong <jiandongsong@126.com>
2017-04-17 10:17:34 +0800

    RecyclerView 学习

---
+++ RecyclerViewTest/app/src/main/java/com/example/dailiang/recyclerviewtest/FruitAdapter.java
@@ -0,0 +1,72 @@
+package com.example.dailiang.recyclerviewtest;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by DaiLiang on 2017/4/15.
+ * 适配器
+ */
+
+class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder>{
+
+    private List<Fruit> mFruitList;
+
+
+    /**
+     * 重写 RecyclerView.Adapter 的 onCreateViewHolder 方法，用于创建 ViewHolder 实例
+     * 把 fruit_item 加载进来，创建一个 ViewHolder 实例
+     */
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fruit_item, parent, false);
+        //ViewHolder holder = new ViewHolder(view);
+        //return holer;
+        return new ViewHolder(view);
+    }
+
+    /**
+     * 对 RecyclerView 子项的数据进行赋值。
+     * @param holder 需要赋值的对象
+     * @param position 得到当前项的 Fruit 实例
+     */
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Fruit fruit = mFruitList.get(position);
+        holder.fruitImage.setImageResource(fruit.getImageId());
+        holder.fruitName.setText(fruit.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mFruitList.size();
+    }
+
+    /**
+     * FruitAdapter 构造器
+     * @param fruitList 需要展示的数据源
+     */
+    FruitAdapter(List<Fruit> fruitList){
+        mFruitList = fruitList;
+    }
+
+    /**
+     * 内部类：ViewHolder 继承自 RecyclerView 的内部类 ViewHolder
+     */
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView fruitImage;
+        TextView fruitName;
+        ViewHolder(View view) {
+            super(view);
+            fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+            fruitName = (TextView) view.findViewById(R.id.fruit_name);
+        }
+    }
+}
