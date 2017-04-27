49e53cf0fdf1315e4e78065cb0452477c6f92a58
JaydenSoong <jiandongsong@126.com>
2017-04-27 11:45:40 +0800

    添加记住密码功能

--- BroadcastBestPractice/app/src/main/java/com/example/dailiang/broadcastbestpractice/LoginActivity.java
+++ BroadcastBestPractice/app/src/main/java/com/example/dailiang/broadcastbestpractice/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.example.dailiang.broadcastbestpractice;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -11,6 +14,11 @@ public class LoginActivity extends BaseActivity {
 
     private EditText accountEdit;
     private EditText passwordEdit;
+
+    private SharedPreferences spf;
+    private SharedPreferences.Editor editor;
+    private CheckBox rememberPass;
+
    // private Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +26,18 @@ public class LoginActivity extends BaseActivity {
         setContentView(R.layout.activity_login);
 
         // 获取实例
+        spf = PreferenceManager.getDefaultSharedPreferences(this);
+        rememberPass = (CheckBox) findViewById(R.id.remember_pass);
         accountEdit = (EditText) findViewById(R.id.account);
         passwordEdit = (EditText) findViewById(R.id.password);
+        boolean isRemember = spf.getBoolean("remember_password", false);
         Button loginButton = (Button) findViewById(R.id.login);
+        if(isRemember) {
+            String account = spf.getString("account", "");
+            String password = spf.getString("password","");
+            accountEdit.setText(account);
+            passwordEdit.setText(password);
+        }
 
         // 为按钮添加处理逻辑
         loginButton.setOnClickListener(new View.OnClickListener() {
@@ -30,6 +47,15 @@ public class LoginActivity extends BaseActivity {
                 String password = passwordEdit.getText().toString();
                 // 若 account 为 admin，password 为 123456，则登陆成功
                 if (account.equals("admin") && password.equals("123456")) {
+                    editor = spf.edit();
+                    if (rememberPass.isChecked()) {
+                        editor.putBoolean("remember_password", true);
+                        editor.putString("account", account);
+                        editor.putString("password", password);
+                    } else {
+                        editor.clear();
+                    }
+                    editor.apply();
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
                     finish();
